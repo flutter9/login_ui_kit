4fd7d733e4b3b8eb636220124e8eb48360c89a38
root <root@ip-172-26-6-84.ap-southeast-1.compute.internal>
2020-03-28 17:15:43 +0000

    build web

---
+++ flutter_service_worker.js
@@ -0,0 +1,36 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/index.html": "e6e25ef07ab061f3396db68372e4cc59",
+"/assets/images/og.png": "88319e2dc428557cdf1f6663c22b87a9",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "9a62a954b81a1ad45a58b9bcea89b50b",
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/AssetManifest.json": "38c48b1253f9d8e4416742229a27b8e8",
+"/assets/LICENSE": "964211db6a8b173b1744e68da77ce459",
+"/main.dart.js": "f109226c320bf36176a7ea662788e594"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
